5b17dd9100f9703060ccc6e809d93a5d649eb5d3
jiro4989 <jiroron666@gmail.com>
2018-08-11 16:27:10 +0900

    特定ディレクトリを指定してデプロイできるように変更した

--- script/build.sh
+++ script/build.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-set -eu
+# build.sh はQTアプリをデプロイするスクリプト
+#
+# 概要
+#   Docker内部から呼び出される想定です。
+#   docker runに本スクリプトを渡すと
+#   docker内でこのスクリプトを実行して
+#   実行可能バイナリを生成します。
+# 引数
+#   target_dir
+#     ビルド対象のソースコードの存在するディレクトリ
+#     引数に渡すのはtkfm配下のディレクトリでtkfmは省略
+#     渡さなければtkfm直下でqtdeployを実行します。
 
-cd $GOPATH/src/github.com/jiro4989/tkfm
+target_dir=$1
+
+if [ -z "$target_dir" ]; then
+  cd $GOPATH/src/github.com/jiro4989/tkfm
+else
+  cd $GOPATH/src/github.com/jiro4989/tkfm/$target_dir
+fi
 qtdeploy
