a10caaaecf3499f01d2e615985dc04aa060d96f9
jiro4989 <jiroron666@gmail.com>
2019-09-12 23:29:58 +0900

    :fire: 使ってないヘッダを削除

--- src/App.js
+++ src/App.js
@@ -56,12 +56,6 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React/Electron</h2>
-      </div>
-      <p className="App-intro">Hello Electron!</p>
-
       <TestCounter />
       <TestTextInput />
       <TestCrop />
